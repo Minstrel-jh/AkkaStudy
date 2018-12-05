b8a1e90b0e73c31aaad08b59a331d3b423dab77b
jiang.han <635481671@qq.com>
2018-12-05 21:17:52 +0800

    config

--- src/main/java/config/Main.java
+++ src/main/java/config/Main.java
@@ -1,11 +1,33 @@
-package dispatcher;
+package config;
 
 import akka.actor.ActorSystem;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+        main.getConfig();
+    }
+
+    public void getConfig() {
+        InputStream rs = this.getClass().getResourceAsStream("application.conf");
+        Properties props = new Properties();
+        try {
+            props.load(rs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String prop = props.getProperty("spark.master");
+        System.out.println(prop);
+    }
+
+    public static void tmp() {
         Config sparkConf = ConfigFactory.load("application.conf").getConfig("spark");
         String master = sparkConf.getString("master");
         String duration = sparkConf.getString("streaming.batch.duration");
