ff430993b4e2aa822e04db1b2c44d141ff1650e9
jiang.han <635481671@qq.com>
2018-12-27 20:44:03 +0800

    对并行流增加一些注释说明

--- src/main/java/dispatcher/demo/ControlActor.java
+++ src/main/java/dispatcher/demo/ControlActor.java
@@ -13,11 +13,19 @@ public class ControlActor extends UntypedActor {
         if (msg instanceof StartCommand) {
             List<ActorRef> actors = createActors(((StartCommand) msg).getActorCount());
 
-            /*这里使用了JDK1.8中的StreamAPI*/
+            /**
+             * 这里使用了JDK1.8中的StreamAPI
+             * parallel() 转变为并行流
+             *
+             * 并行调用列表中的actor发消息
+             */
             actors.stream().parallel().forEach(actorRef -> actorRef.tell("Insert", ActorRef.noSender()));
         }
     }
 
+    /**
+     * 根据StartCommand中的actorCount创建了这么多个Actor的一个列表
+     */
     private List<ActorRef> createActors(int actorCount) {
         Props props = Props.create(WriterActor.class).withDispatcher("writer-dispatcher");
         ArrayList<ActorRef> actors = new ArrayList<>(actorCount);
